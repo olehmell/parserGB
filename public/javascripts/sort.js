abe78bcd81192dafcae5b9f634e65877a3730f61
Mell <oleh.melnechyk@gmail.com>
2019-05-12 12:21:27 +0300

    finish sort

--- public/javascripts/sort.js
+++ public/javascripts/sort.js
@@ -1,24 +1,51 @@
 
-$(".full_info").click(function () {
-    let valClass = (this.className).split(' ')[1];
-    console.log(valClass);
-    sort(valClass);
+$(".sortUp").on("click",function () {
+    let $this = $(this);
+    let valClass = ($this.parent());
+    $this.toggleClass("down");
+    sort(valClass[0].className.split(' ')[1], $this.hasClass('down'));
 });
-function sort(valClass) {
+function sort(valClass,down) {
     let elements = $(".sort_rows");
-    console.log(elements);
-    let element = bubbleSort(elements,valClass);
-    //console.log(Number($(elements[2]).find(`.${valClass}`).text()));
-    console.log(element);
-    //$(".table_project").append(elements);
-    //$(".sort_rows").remove();
+    $(".hide").remove();
+    let elementSort;
+    if(!down)
+    {
+        elementSort = bubbleSortUp(elements.clone(),valClass,elements);
+        for (let i = 0; i < Object.keys(elements).length - 2;i++)
+        {
+            elements[i].replaceWith(elementSort[i+1]);
+        }
+    }
+    else
+    {
+        elementSort = bubbleSortDown(elements.clone(),valClass,elements);
+        for (let i = 0; i < Object.keys(elements).length - 2;i++)
+        {
+            elements[i].replaceWith(elementSort[i]);
+        }
+    }
+
+
 
 }
 
-function bubbleSort(arr,valClass) {
+function bubbleSortUp(arr,valClass) {
     for (let i = 0, endI = Object.keys(arr).length - 2; i < endI; i++) {
         for (let j = 0, endJ = endI - i; j < endJ; j++) {
             if (Number($(arr[j]).find(`.${valClass}`).text()) > Number($(arr[j+1]).find(`.${valClass}`).text())) {
+                const swap = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = swap;
+            }
+        }
+    }
+    return arr;
+}
+function bubbleSortDown(arr,valClass) {
+    for (let i = 0, endI = Object.keys(arr).length - 2; i < endI; i++) {
+        for (let j = 0, endJ = endI - i; j < endJ; j++) {
+            if (Number($(arr[j]).find(`.${valClass}`).text()) < Number($(arr[j+1]).find(`.${valClass}`).text())) {
                 const swap = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = swap;
