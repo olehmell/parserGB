5eb4a62f9013831fa607c53302f4e3d8efb63a04
Mell <oleh.melnechyk@gmail.com>
2019-05-15 22:27:14 +0300

    FIX

--- bin/db.js
+++ bin/db.js
@@ -8,10 +8,10 @@ const conn = mysql.createConnection({
 });
 /*
 /*
-database: 'zcaj2nq68420bkec',
-    host: "lt80glfe2gj8p5n2.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
-    user: "ljbyzr3bxwkhl203",
-    password: "dcjm126j417bn8b5"
+database: 'mag6fchr4qon9rwk',
+    host: "irkm0xtlo2pcmvvz.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
+    user: "p89znwt5dr9w2k6x",
+    password: "qfoepz8xcvsv4qme"
  */
 
 const dataPages = ["project.json"]
@@ -22,9 +22,12 @@ conn.connect(function (err) {
     console.log("Connected!");
     //console.log(table);
     //const sql = "drop table projects;"
-    //const sql = "select * from projects;"
+
+
+
+    const sql = "select * from projects;"
     //const sql = "ALTER TABLE projects ADD pr3333 INT NULL default 0;";
-    //conn.query(sql,(err,result) => console.log(result))
+    conn.query(sql,(err,result) => console.log(result))
     const sql0 = "SHOW TABLES LIKE 'projects';"
     function createTable() {
         let table = "";
