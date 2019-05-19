0949887b752fe1174509f8b2a5c9d62a5cab45b2
Mell <oleh.melnechyk@gmail.com>
2019-05-19 10:02:34 +0300

    ifu

--- bin/db.js
+++ bin/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 const fs = require('fs');
 let conn = mysql.createPool({
     connectionLimit : 5,
-    database: 'dbForGB',
-    host: "localhost",
-    user: "root",
-    password: "admin"
+    host     : 'eu-cdbr-west-02.cleardb.net',
+    user     : 'b00300329a8eb3',
+    password : '88e479ab',
+    database : 'heroku_66b709597cff373'
 });
 /*
 /*
@@ -18,17 +18,10 @@ database: 'dbForGB',
     password : '9b98f6a3',
     database : 'heroku_48ee6185f3d9445'
     database: 'bym1onf7s6lbdh4v',
-    host: "sabaik6fx8he7pua.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
-    user: "z6wyzx8uf6w1ynqb",
-    password: "kwcvgb973luqjqae"
-database: 'zcaj2nq68420bkec',
-    host: "lt80glfe2gj8p5n2.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
-    user: "ljbyzr3bxwkhl203",
-    password: "dcjm126j417bn8b5"
-database: 'mag6fchr4qon9rwk',
-    host: "irkm0xtlo2pcmvvz.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
-    user: "p89znwt5dr9w2k6x",
-    password: "qfoepz8xcvsv4qme"
+    host     : 'eu-cdbr-west-02.cleardb.net',
+    user     : 'b00300329a8eb3',
+    password : '88e479ab',
+    database : 'heroku_66b709597cff373'
  */
 const data = JSON.parse(fs.readFileSync(`views/project.json`));
 
