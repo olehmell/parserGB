a0e51a6cf00ff828872dd91bec02dd8b5523395d
Mell <oleh.melnechyk@gmail.com>
2019-05-25 15:22:22 +0300

    please fixed

--- bin/main.js
+++ bin/main.js
@@ -29,8 +29,10 @@ function retung() {
             console.log("ret" + value.retng);
         });
     }).catch(function (err) {
-        //insert();
-        console.log("Произошла ошибка gorin: " + err);});
+        console.log("Произошла ошибка gorin: " + err);
+        retung();
+    });
+
 }
 
 
@@ -51,14 +53,14 @@ function insert() {
     const insertSql = `INSERT INTO projects (${arr}) VALUES(${vall});`;
     //console.log(insertSql);
     //conn.getConnection(function (err, conn) {
-        conn.query(insertSql, function (err, results) {
-            //conn.destroy();
-            if (err)
-                throw err;
-            else
-                console.log("insert to finish");
-        });
-   //});
+    conn.query(insertSql, function (err, results) {
+        //conn.destroy();
+        if (err)
+            throw err;
+        else
+            console.log("insert to finish");
+    });
+    //});
     const time = [3, 12, 36, 72, 144, 288];
     time.forEach(function (value, index) {
         //console.log("run");
@@ -66,77 +68,78 @@ function insert() {
         const sumSQL = `SELECT * FROM (SELECT * FROM projects ORDER BY id DESC LIMIT 0 , ${value}) t ORDER BY id ASC;`
         //console.log(sumSQL);
         //conn.getConnection(function (err, conn) {
-            conn.query(sumSQL, function (err, results) {
-                //conn.destroy();
-                if (err) throw err;
-                else {
-                    switch (value) {
-                        case 3:
-                            data.forEach(function (value1, index1) {
-                                value1.ten = (results[results.length - 1][`pr${value1.number}`] - results[0][`pr${value1.number}`]);
-                                //console.log(value1.ten);
-                            });
-                            break;
-                        case 12:
-                            data.forEach(function (value1, index1) {
-                                value1.hour1 = (results[results.length - 1][`pr${value1.number}`] - results[0][`pr${value1.number}`]);
-                            });
-                            break;
-                        case 36:
-                            data.forEach(function (value1, index1) {
-                                value1.hour3 = (results[results.length - 1][`pr${value1.number}`] - results[0][`pr${value1.number}`]);
-                            });
-                            break;
-                        case 72:
-                            data.forEach(function (value1, index1) {
-                                value1.hour6 = (results[results.length - 1][`pr${value1.number}`] - results[0][`pr${value1.number}`]);
-                            });
-                            break;
-                        case 144:
-                            data.forEach(function (value1, index1) {
-                                value1.hour12 = (results[results.length - 1][`pr${value1.number}`] - results[0][`pr${value1.number}`]);
-                            });
-                            break;
-                        case 288:
-                            data.forEach(function (value1, index1) {
-                                value1.hour24 = (results[results.length - 1][`pr${value1.number}`] - results[0][`pr${value1.number}`]);
-                            });
-                            break;
-                    }
-                }
-            });
-        });
-   // });
-
-    const sqlSelect = `SELECT * from projects`;
-    //conn.getConnection(function (err, conn) {
-        conn.query(sqlSelect, function (err, result) {
+        conn.query(sumSQL, function (err, results) {
             //conn.destroy();
             if (err) throw err;
             else {
-                data.forEach(function (valueD, indexD) {
-                    let mass = [], massSUM = [], massRg = [];
-                    for (let index = 0; index < result.length; index += 12) {
-                        massSUM.push(result[index][`pr${valueD.number}`]);
-                        //assRg.push(result[index][`pr${valueD.number}`]);
-                        //console.log(massRg);
-                    }
-                    for (let index = 0; index < result.length; index += 12) {
-                        massRg.push(Math.round(result[index][`pr${valueD.number}`] / valueD.amount * 10000000));
-                        //console.log(massRg);
-                    }
-
-                    for (let time = 1; time < result.length; time += 2) {
-                        mass.push(result[time][`pr${valueD.number}`] - result[time - 1][`pr${valueD.number}`]);
-                    }
-                    data[indexD].data = mass;
-                    data[indexD].dataSUM = massSUM;
-                    data[indexD].dataRg = massRg;
-                });
+                switch (value) {
+                    case 3:
+                        data.forEach(function (value1, index1) {
+                            value1.ten = (results[results.length - 1][`pr${value1.number}`] - results[0][`pr${value1.number}`]);
+                            //console.log(value1.ten);
+                        });
+                        break;
+                    case 12:
+                        data.forEach(function (value1, index1) {
+                            value1.hour1 = (results[results.length - 1][`pr${value1.number}`] - results[0][`pr${value1.number}`]);
+                        });
+                        break;
+                    case 36:
+                        data.forEach(function (value1, index1) {
+                            value1.hour3 = (results[results.length - 1][`pr${value1.number}`] - results[0][`pr${value1.number}`]);
+                        });
+                        break;
+                    case 72:
+                        data.forEach(function (value1, index1) {
+                            value1.hour6 = (results[results.length - 1][`pr${value1.number}`] - results[0][`pr${value1.number}`]);
+                        });
+                        break;
+                    case 144:
+                        data.forEach(function (value1, index1) {
+                            value1.hour12 = (results[results.length - 1][`pr${value1.number}`] - results[0][`pr${value1.number}`]);
+                        });
+                        break;
+                    case 288:
+                        data.forEach(function (value1, index1) {
+                            value1.hour24 = (results[results.length - 1][`pr${value1.number}`] - results[0][`pr${value1.number}`]);
+                        });
+                        break;
+                }
             }
         });
-   // });
+    });
+    // });
+
+    const sqlSelect = `SELECT * from projects`;
+    //conn.getConnection(function (err, conn) {
+    conn.query(sqlSelect, function (err, result) {
+        //conn.destroy();
+        if (err) throw err;
+        else {
+            data.forEach(function (valueD, indexD) {
+                let mass = [], massSUM = [], massRg = [];
+                for (let index = 0; index < result.length; index += 12) {
+                    massSUM.push(result[index][`pr${valueD.number}`]);
+                    //assRg.push(result[index][`pr${valueD.number}`]);
+                    //console.log(massRg);
+                }
+                for (let index = 0; index < result.length; index += 12) {
+                    massRg.push(Math.round(result[index][`pr${valueD.number}`] / valueD.amount * 10000000));
+                    //console.log(massRg);
+                }
+
+                for (let time = 1; time < result.length; time += 2) {
+                    mass.push(result[time][`pr${valueD.number}`] - result[time - 1][`pr${valueD.number}`]);
+                }
+                data[indexD].data = mass;
+                data[indexD].dataSUM = massSUM;
+                data[indexD].dataRg = massRg;
+            });
+        }
+    });
+    // });
 }
+
 function parse() {
     let options = {
         uri: "https://gurin.com.ua/rating.php",
@@ -152,8 +155,7 @@ function parse() {
             i++;
             let suffrage = $(".votes-count").find("strong").html();
             suffrage = suffrage.split(' ');
-            if(suffrage.length > 1)
-            {
+            if (suffrage.length > 1) {
                 suffrage = suffrage[0] + suffrage[1].toString();
             }
             if (Number(suffrage) >= valueD.suffrage)
@@ -169,6 +171,7 @@ function parse() {
         })
     })
 }
+
 /*function parse()
 {
     let options = {
