fd70487d23223aea4546a32f4ca7dcc3582c3382
Mell <oleh.melnechyk@gmail.com>
2019-06-03 18:04:33 +0300

    maibe fix

--- bin/db.js
+++ bin/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 const fs = require('fs');
 let conn = mysql.createPool({
     connectionLimit : 10,
-    host     : 'eu-cdbr-west-02.cleardb.net',
-    user     : 'b077bcc556224b',
-    password : '9b98f6a3',
-    database : 'heroku_48ee6185f3d9445'
+    host     : 'sabaik6fx8he7pua.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
+    user     : 'z6wyzx8uf6w1ynqb',
+    password : 'kwcvgb973luqjqae',
+    database : 'bym1onf7s6lbdh4v'
 });
 /*
 /*
@@ -26,10 +26,10 @@ database: 'dbForGB',
 const data = JSON.parse(fs.readFileSync(`views/project.json`));
 
     //console.log(table);
-    const sql = "drop table projects;"
+    //const sql = "drop table projects;"
     //const sql = "select * from projects;"
     //const sql = "alter table projects add time DATETIME DEFAULT CURRENT_TIMESTAMP;";
-    conn.query(sql, (err, result) => console.log(result))
+    //conn.query(sql, (err, result) => console.log(result))
     const sql0 = "SHOW TABLES LIKE 'projects';"
 
     function createTable() {
