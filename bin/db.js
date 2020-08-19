d1ff1f1fb6704b4d9f2aafe63dcf2654ea6a80ea
olehmell <oleh.melnechyk@gmail.com>
2020-08-19 10:09:40 +0300

    more connecting limit

--- bin/db.js
+++ bin/db.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 const fs = require("fs");
 let conn = mysql.createPool({
-  connectionLimit: 10,
+  connectionLimit: 50,
   host: "eu-cdbr-west-03.cleardb.net",
   user: "b6f0316e1ff69d",
   password: "ef4627abc83df7f",
